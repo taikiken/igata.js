afa7a9027e0c87770c6b15158115078b071f0718
Hiroaki Okubo <denkosekka@gmail.com>
2015-09-14 08:22:20 +0900

    API

---
+++ docs/api.js
@@ -0,0 +1,65 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Affine2d",
+        "Angle",
+        "Bbf",
+        "Bitmap",
+        "Blur",
+        "Brightness",
+        "Cache",
+        "Calc",
+        "Cascade",
+        "Convolution",
+        "Data_t",
+        "Estimator",
+        "FastCorner",
+        "Filter",
+        "Grayscale",
+        "Haar",
+        "HomoGraphy2d",
+        "IVector",
+        "Igata",
+        "Invert",
+        "Kanade",
+        "Keypoint_t",
+        "LA",
+        "LinearAlgebra",
+        "MM",
+        "MatrixMath",
+        "Matrix_t",
+        "MotionEstimator",
+        "Node_t",
+        "Orb",
+        "Processing",
+        "Pyramid_t",
+        "Ransac_t",
+        "Sepia",
+        "Stack",
+        "Table_t",
+        "Threshold",
+        "Vector2",
+        "Vector3",
+        "Yape",
+        "Yape06",
+        "lev_table_t",
+        "perform_one_point"
+    ],
+    "modules": [
+        "Feat",
+        "Igata"
+    ],
+    "allModules": [
+        {
+            "displayName": "Feat",
+            "name": "Feat",
+            "description": "original code\nhttp://inspirit.github.io/jsfeat/"
+        },
+        {
+            "displayName": "Igata",
+            "name": "Igata"
+        }
+    ],
+    "elements": []
+} };
+});
